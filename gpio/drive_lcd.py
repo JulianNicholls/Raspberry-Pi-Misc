95d6d84ef64c71f01c4382830680d576f2ec0fb8
Julian Nicholls <juliannicholls29@gmail.com>
2015-09-14 14:08:23 +0000

    Update drive_lcd.py
    Add Home, Clear, text. and cursor positioning functions.
    
    It still doesn't work reliably, but I don;t know if this is
    (a) A dodgy physical connection.
    (b) A dodgy reading of the GPIO pinout and usage for the Pi 2.
    (c) A software problem.
    
    I'm leaning away from (c) because I've done this before and the timings
    are stupidly long rather than too tight.

--- gpio/drive_lcd.py
+++ gpio/drive_lcd.py
@@ -9,7 +9,6 @@ db5_pin = 16    # DB5
 db6_pin = 18    # DB6
 db7_pin = 22    # DB7
 
-
 def setup_pins():
     GPIO.setmode(GPIO.BOARD)
 
@@ -20,6 +19,40 @@ def setup_pins():
     GPIO.setup(db6_pin, GPIO.OUT)
     GPIO.setup(db7_pin, GPIO.OUT)
 
+
+def setup_lcd():
+    write_4_ins(2)
+    write_4_ins(2)
+    write_4_ins(8)      # 2-line, 5x7
+    time.sleep(0.01)
+    
+    write_4_ins(0)
+    write_4_ins(0xE)    # Display on, cursor on, blink off
+    time.sleep(0.01)
+
+    write_4_ins(0)      # Clear display
+    write_4_ins(1)
+    time.sleep(0.01)
+
+    write_4_ins(0)
+    write_4_ins(6)      # Increment on, shift off
+    time.sleep(0.01)
+
+def clear():
+    write_8_ins(0x01)
+
+def home():
+    write_8_ins(0x02)
+
+def set_position(y, x):
+    set_address_raw(y * 0x40 + x)
+
+def set_address_raw(addr):
+    write_8_ins(0x80 + addr)
+
+def say(text):
+    for i in text:
+        write_8_data(ord(i))
 
 def write_8_ins(value):
     write_4_ins(value >> 4)
@@ -49,38 +82,35 @@ def write_4(value):
 
 
 def strobe_e():
-    time.sleep(0.01)  # Limit is min 500ns, so this is plenty
+    time.sleep(0.001)  # Limit is min 500ns, so this is plenty
     GPIO.output(e_pin, GPIO.HIGH)
-    time.sleep(0.01)  # Limit is min 500ns, so this is plenty
+    time.sleep(0.001)  # Limit is min 500ns, so this is plenty
     GPIO.output(e_pin, GPIO.LOW)
-    time.sleep(0.01)  # Limit is min 500ns, so this is plenty
-
-
-def setup_lcd():
-    write_4_ins(2)
-    write_4_ins(2)
-    write_4_ins(8)      # 2-line, 5x7
-    time.sleep(0.01)
-    
-    write_4_ins(0)
-    write_4_ins(0xE)    # Display on, cursor off
-    time.sleep(0.01)
-
-    write_4_ins(0)      # Clear display
-    write_4_ins(1)
-    time.sleep(0.01)
-
-    write_4_ins(0)
-    write_4_ins(6)      # Increment on, shift off
+    time.sleep(0.001)  # Limit is min 500ns, so this is plenty
 
 
 setup_pins()
 setup_lcd()
 
 write_8_data(0x45)
-# write_8_data(0x46)
-# write_8_data(0x47)
+write_8_data(0x46)
+write_8_data(0x47)
+wait = raw_input()
 
+clear()
+say("Clear");
+wait = raw_input()
+
+home()
+say("Home...");
+wait = raw_input()
+
+set_address_raw(0x40)
+say("Second Line");
+wait = raw_input()
+
+set_position(1, 0xD)
+say("End");
 wait = raw_input()
 
 GPIO.cleanup()
