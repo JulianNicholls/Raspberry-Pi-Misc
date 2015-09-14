c72466715ddb0fe363fc89886efbf5df83c4cb22
Julian Nicholls <juliannicholls29@gmail.com>
2015-09-14 17:33:23 +0100

    Update README and drive_lcd comments

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
-# Raspberry-Pi-Misc
+# Raspberry Pi Miscellany
 
 Files from my initial forays into programming for the [Raspberry Pi](http://raspberrypi.org).
+
+## GPIO
+
+A few programs to flash LEDs etc.
+
+### flash12
+
+Flash an LED attached to Pin 12 (GPIO18).
+
+### flash_12_26
+
+Flash two LEDs alternately, attached to pins 12 and 26 (GPIO18 & GPIO7).
+
+### drive_speaker
+
+Make a 1kHz (ish) tone from a speaker attached to pin 12 (GPIO18). I'm not sure 
+of the actual resolution of time.sleep() so I don't know if asking for a 1ms 
+delay is sensible.
+
+### drive_lcd
+
+Drive a 16x2 LCD display in a 4-bit configuration, attached to GPIO pins (See
+inside for the data pins).  The program starts by initializing the panel and 
+writing 'EFG'. Then it waits for the user to hit enter between each of the 
+next phases: clear the display and write 'Clear', home the cursor and write 
+'Home...', position the cursor to the beginning of the second line and write 
+'Second Line', then finally write 'End' at the end of the second line, before 
+finally waiting for the user to hit enter again and then disconnecting from
+GPIO.
+
+## PYGAME
+
+### bounce1
+
+Bounce a (square!) ball around in a window.
+
+### bounce2
+
+Bounce a ball around in a box with a bouncy sound on each bounce.
+
--- gpio/drive_lcd.py
+++ gpio/drive_lcd.py
@@ -1,5 +1,24 @@
+# Standard 16x2 / 16x4 / 20x4 LCD Pinout
+
+# 1   GND       Ground
+# 2   Vin       +2.7-5.5v apparently, but it seems to need +5v
+# 3   Vss       LCD Power
+# 4   RS        _Instruction / Data
+# 5   R/_W      Tie low for writing
+# 6   E         Strobe High to set written data
+# 7   DB0       Data Bus bit 0 - Not Connected
+# 8   DB1       Data Bus bit 1 - Not Connected
+# 9   DB2       Data Bus bit 2 - Not Connected
+#10   DB3       Data Bus bit 3 - Not Connected
+#11   DB4       Data Bus bit 4 
+#12   DB5       Data Bus bit 5 
+#13   DB6       Data Bus bit 6 
+#14   DB7       Data Bus bit 7 
+
 import RPi.GPIO as GPIO
 import time
+
+#   GPIO Pins used
 
 rs_pin  = 26    # RS !INS/DATA
 e_pin   = 24    # E
@@ -8,6 +27,8 @@ db4_pin = 12    # DB4
 db5_pin = 16    # DB5
 db6_pin = 18    # DB6
 db7_pin = 22    # DB7
+
+# Set up all the above pins for output
 
 def setup_pins():
     GPIO.setmode(GPIO.BOARD)
@@ -19,6 +40,7 @@ def setup_pins():
     GPIO.setup(db6_pin, GPIO.OUT)
     GPIO.setup(db7_pin, GPIO.OUT)
 
+# Initialize the LCD for 4-bit operation
 
 def setup_lcd():
     write_4_ins(2)
@@ -38,25 +60,44 @@ def setup_lcd():
     write_4_ins(6)      # Increment on, shift off
     time.sleep(0.01)
 
+# Clear the display and home the cursor to top-left
+
 def clear():
     write_8_ins(0x01)
+
+# Just home the cursor to the top-left without clearing the display
 
 def home():
     write_8_ins(0x02)
 
+# Set the cursor position by row and column, this only works for 16x2 and 20x2
+# at the moment because I don't have a 16x4 or 20x4 LCD, or the docs. I recall 
+# that line 2 runs on from line 0, and line 3 runs on from line 1, but I don't
+# remember whether there is any gap in addressing.
+
 def set_position(y, x):
     set_address_raw(y * 0x40 + x)
 
+# Set the address in raw mode, line 0 starts at offset 0x00 and line 1 starts 
+# at 0x40.
+
 def set_address_raw(addr):
     write_8_ins(0x80 + addr)
+
+# Output a piece of text, no note is taken about running off the right side.
 
 def say(text):
     for i in text:
         write_8_data(ord(i))
 
+# Write an 8-bit value as an instruction, just writes the top 4 bits followed 
+# by the bottom 4 bits
+
 def write_8_ins(value):
     write_4_ins(value >> 4)
     write_4_ins(value & 0x0F)
+
+# Write a 4-bit value as an instruction.
 
 def write_4_ins(value):
     write_4(value)
@@ -64,15 +105,21 @@ def write_4_ins(value):
     strobe_e()
 
 
+# Write an 8-bit value as a piece of data (text), just writes the top 4 bits 
+# followed by the bottom 4 bits
+
 def write_8_data(value):
     write_4_data(value >> 4)
     write_4_data(value & 0x0F)
+
+# Write a 4-bit value as a piece of data (text).
 
 def write_4_data(value):
     write_4(value)
     GPIO.output(rs_pin, GPIO.HIGH)
     strobe_e()
 
+# Write a nybble to the 4 data lines
 
 def write_4(value):
     GPIO.output(db4_pin, GPIO.HIGH if ((value & 1) == 1) else GPIO.LOW)
@@ -80,6 +127,8 @@ def write_4(value):
     GPIO.output(db6_pin, GPIO.HIGH if ((value & 4) == 4) else GPIO.LOW)
     GPIO.output(db7_pin, GPIO.HIGH if ((value & 8) == 8) else GPIO.LOW)
 
+# Strobe the E lkine high for a minimum of 500ns. As can be seen, it is held
+# high for 1ms.
 
 def strobe_e():
     time.sleep(0.001)  # Limit is min 500ns, so this is plenty
@@ -88,30 +137,30 @@ def strobe_e():
     GPIO.output(e_pin, GPIO.LOW)
     time.sleep(0.001)  # Limit is min 500ns, so this is plenty
 
+if __name__ == '__main__':
+    setup_pins()
+    setup_lcd()
 
-setup_pins()
-setup_lcd()
-
-write_8_data(0x45)
-write_8_data(0x46)
-write_8_data(0x47)
-wait = raw_input()
-
-clear()
-say("Clear");
-wait = raw_input()
-
-home()
-say("Home...");
-wait = raw_input()
-
-set_address_raw(0x40)
-say("Second Line");
-wait = raw_input()
-
-set_position(1, 0xD)
-say("End");
-wait = raw_input()
-
-GPIO.cleanup()
-
+    write_8_data(0x45)
+    write_8_data(0x46)
+    write_8_data(0x47)
+    wait = raw_input()
+    
+    clear()
+    say("Clear");
+    wait = raw_input()
+    
+    home()
+    say("Home...");
+    wait = raw_input()
+    
+    set_address_raw(0x40)
+    say("Second Line");
+    wait = raw_input()
+    
+    set_position(1, 0xD)
+    say("End");
+    wait = raw_input()
+    
+    GPIO.cleanup()
+    
