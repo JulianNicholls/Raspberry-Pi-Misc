a0f4a364e2a5ae59a194a46f234ef7c71c859d40
Julian Nicholls <juliannicholls29@gmail.com>
2015-09-22 15:23:02 +0100

    Update LCD driver library.
    setup_pins() and setup_lcd() have been amalgamated into setup()
    Added cursor setting.
    Changed tests run.

--- gpio/drive_lcd.py
+++ gpio/drive_lcd.py
@@ -28,9 +28,17 @@ db5_pin = 16    # DB5
 db6_pin = 18    # DB6
 db7_pin = 22    # DB7
 
-# Set up all the above pins for output
 
-def setup_pins():
+# Cursor settings
+
+OFF         = 0x04
+ON          = 0x06
+BLINKING    = 0x07
+
+# Set up all the above pins for output and initialise the display for 
+# 4-bit operation
+
+def setup():
     GPIO.setmode(GPIO.BOARD)
 
     GPIO.setup(rs_pin, GPIO.OUT)
@@ -40,9 +48,6 @@ def setup_pins():
     GPIO.setup(db6_pin, GPIO.OUT)
     GPIO.setup(db7_pin, GPIO.OUT)
 
-# Initialize the LCD for 4-bit operation
-
-def setup_lcd():
     write_4_ins(2)
     write_4_ins(2)
     write_4_ins(8)      # 2-line, 5x7
@@ -60,6 +65,9 @@ def setup_lcd():
     write_4_ins(6)      # Increment on, shift off
     time.sleep(0.01)
 
+def release_pins():
+    GPIO.cleanup()
+
 # Clear the display and home the cursor to top-left
 
 def clear():
@@ -69,6 +77,9 @@ def clear():
 
 def home():
     write_8_ins(0x02)
+
+def set_cursor(setting):
+    write_8_ins(0x08 + setting)
 
 # Set the cursor position by row and column, this only works for 16x2 and 20x2
 # at the moment because I don't have a 16x4 or 20x4 LCD, or the docs. I recall 
@@ -127,7 +138,7 @@ def write_4(value):
     GPIO.output(db6_pin, GPIO.HIGH if ((value & 4) == 4) else GPIO.LOW)
     GPIO.output(db7_pin, GPIO.HIGH if ((value & 8) == 8) else GPIO.LOW)
 
-# Strobe the E lkine high for a minimum of 500ns. As can be seen, it is held
+# Strobe the E line high for a minimum of 500ns. As can be seen, it is held
 # high for 1ms.
 
 def strobe_e():
@@ -138,22 +149,24 @@ def strobe_e():
     time.sleep(0.001)  # Limit is min 500ns, so this is plenty
 
 if __name__ == '__main__':
-    setup_pins()
-    setup_lcd()
+    setup()
 
-    write_8_data(0x45)
-    write_8_data(0x46)
-    write_8_data(0x47)
+    write_8_data(0x53)  # 'S'
+    write_8_data(0x45)  # 'E'
+    write_8_data(0x54)  # 'T'
+    write_8_data(0x55)  # 'U'
+    write_8_data(0x50)  # 'P'
     wait = raw_input()
+    
+#    clear()
+#    say("Clear");
+#    wait = raw_input()
+    
+#    home()
+#    say("Home...");
+#    wait = raw_input()
     
     clear()
-    say("Clear");
-    wait = raw_input()
-    
-    home()
-    say("Home...");
-    wait = raw_input()
-    
     set_address_raw(0x40)
     say("Second Line");
     wait = raw_input()
@@ -162,5 +175,22 @@ if __name__ == '__main__':
     say("End");
     wait = raw_input()
     
-    GPIO.cleanup()
+    clear()
+    set_cursor(ON)
+    say("Cursor ON ->")
+    wait = raw_input()
+
+    set_cursor(OFF)
+    home()
+    say("Cursor OFF->")
+    wait = raw_input()
+
+    set_cursor(BLINKING)
+    home()
+    say("Blinking  ->")
+    wait = raw_input()
+
+    set_cursor(OFF)
+
+    release_pins()
     
