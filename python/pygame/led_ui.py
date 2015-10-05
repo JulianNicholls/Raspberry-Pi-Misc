51c2ea93c621229cbd31d95c1aad23ffa2470d41
Julian Nicholls <juliannicholls29@gmail.com>
2015-10-05 22:32:21 +0100

    Re-factor Button and Pin classes.

--- python/pygame/led_ui.py
+++ python/pygame/led_ui.py
@@ -35,54 +35,80 @@ green   = (50, 255, 50)
 
 
 #----------------------------------------------------------------------------
-# GPIO Pin class
+# GPIO Pin class that holds a GPIO pin number, current state, and active
+# low status.
 
 class Pin(object):
     def __init__(self, number, active = False, active_low = False):
+        """ Initialise with the pin number, start state, and active low status """
         self.number     = number
         self.active     = active
         self.active_low = active_low
+
         self.set_state()
 
     def set_state(self):
-        if self.active != self.active_low:
-            gpio.output(self.number, gpio.HIGH)
-        else:
-            gpio.output(self.number, gpio.LOW)
+        """ Set the hardware pin based on state and active low """
+        hilo = gpio.HIGH if self.active != self.active_low else gpio.LOW
+        gpio.output(self.number, hilo)
 
     def toggle(self):
+        """ Toggle the state and action it """
         self.active = not self.active
         self.set_state()
 
 
 #----------------------------------------------------------------------------
-# Button Class
+# Generic Button class that knows how to draw itself and returns true if it
+# has been clicked.
 
-class PinButton(object):
-    def __init__(self, x, y, pin, text, colour, width = 200, height = 100, x_offset = 50, y_offset = 30):
-        self.x          = x
-        self.y          = y
-        self.pin        = pin
+class Button(object):
+    def __init__(self, x, y, text, colour, width, height, x_offset, y_offset):
+        self.x, self.y          = x, y
+        self.width, self.height = width, height
+        self.x_off, self.y_off  = x_offset, y_offset
+
         self.text       = text
         self.colour     = colour
-        self.width      = width
-        self.height     = height
-        self.x_offset   = x_offset
-        self.y_offset   = y_offset
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.colour if self.pin.active else off, (self.x, self.y, self.width, self.height))
+        """ Draw the button rectangle and its text """
+        pygame.draw.rect(screen, colour, (self.x, self.y, self.width, self.height))
 
-        text_surface = button_font.render(self.text, 1, black if self.pin.active else white) 
-        screen.blit(text_surface, (self.x + self.x_offset, self.y + self.y_offset))
+        text_surface = button_font.render(self.text, 1, white) 
+        screen.blit(text_surface, (self.x + self.x_off, self.y + self.y_off))
 
-    def clicked(self, pos):
+    def contains(self, pos):
+        """ Return whether the passed position is part of the button """
         return pos[0] >= self.x and pos[0] < self.x + self.width and \
                pos[1] >= self.y and pos[1] < self.y + self.height
 
+    def action(self):
+        """ NULL Action """
+        pass
 
 #----------------------------------------------------------------------------
-# Initialisation
+# PinButton Class that holds a Pin reference and draws based on it.
+
+class PinButton(Button):
+    def __init__(self, x, y, pin, text, colour, width = 200, height = 100, x_offset = 50, y_offset = 30):
+        super().__init__(x, y, text, colour, width, height, x_offset, y_offset) 
+
+        self.pin = pin
+
+    def draw(self, screen):
+        """ Draw the button rectangle and its text sewnsitive to pin status """
+        pygame.draw.rect(screen, self.colour if self.pin.active else off, (self.x, self.y, self.width, self.height))
+
+        text_surface = button_font.render(self.text, 1, black if self.pin.active else white) 
+        screen.blit(text_surface, (self.x + self.x_off, self.y + self.y_off))
+
+    def action(self):
+        """ Toggle the pin state """
+        self.pin.toggle()
+
+#----------------------------------------------------------------------------
+# Initialisation functions
 
 def initialise_gpio():
     gpio.setmode(gpio.BOARD)
@@ -110,7 +136,7 @@ initialise_window()
 screen      = pygame.display.set_mode(window_size)
 button_font = pygame.font.Font(None, 48)
 
-buttons    = [
+buttons     = [
     PinButton( 20,  20, Pin(red_pin),   'Red',   red,   200, 100, 60, 30),
     PinButton(240,  20, Pin(amber_pin), 'Amber', amber, 200, 100, 50, 30),
     PinButton(460,  20, Pin(green_pin), 'Green', green, 200, 100, 50, 30),
@@ -121,7 +147,7 @@ buttons    = [
     PinButton( 50, 355, Pin(pins7[6], False, True), 'g', red, 150,  35, 60,  -4),
     PinButton( 33, 400, Pin(pins7[3], False, True), 'd', red,  35, 150,  7, 50),
     PinButton(188, 400, Pin(pins7[4], False, True), 'e', red,  35, 150,  7, 50),
-    PinButton( 50, 560, Pin(pins7[5], False, True), 'f', red, 150,  35, 60,  -4),
+    PinButton( 50, 560, Pin(pins7[5], False, True), 'f', red, 150,  35, 62,  -2),
 ]
 
 # Event Loop
@@ -135,8 +161,8 @@ while running:
         running = False
     elif event.type == pygame.MOUSEBUTTONDOWN:
         for b in buttons:
-            if b.clicked(event.pos):
-                b.pin.toggle()
+            if b.contains(event.pos):
+                b.action()
                 break
 
     screen.fill(blue)
