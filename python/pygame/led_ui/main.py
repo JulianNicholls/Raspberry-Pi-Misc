209392ec40b6084d8ea2a62af68f9ef53bbb2d63
Julian Nicholls <juliannicholls29@gmail.com>
2015-10-06 19:30:45 +0100

    Re-factor setup

--- python/pygame/led_ui/main.py
+++ python/pygame/led_ui/main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 """
-PyGame UI for the traffic lights LEDs
-Version 2
+PyGame UI for connected LEDs
+Version 3
 """
 
 import pygame
@@ -20,9 +20,16 @@ green_pin   = 15    # Pin Number, GPIO 22
 blue_pin    = 29    # Pin Number, GPIO 5    
 white_pin   = 31    # Pin Number, GPIO 6
 
-# 7-segment pins (See drive_7_segment.py for more details)
-#              a   b   c   d   e   f   g   p5
-pins7       = [16, 22, 12, 18, 32, 38, 36, 35]
+# 7-segment pins (See .../gpio/drive_7_segment.py for more details)
+
+a_pin       = 16
+b_pin       = 22
+c_pin       = 12
+d_pin       = 18
+e_pin       = 32
+f_pin       = 38
+g_pin       = 36
+p5_pin      = 35
 
 # Sizes and stuff
 
@@ -30,7 +37,7 @@ window_size = (750, 620)
 
 # Colours
 
-blue    = (0, 0, 64)
+blue    = (0, 0, 48)
 white   = (255, 255, 255)
 black   = (0, 0, 0) 
 off     = (32, 32, 32)
@@ -46,15 +53,14 @@ lblue   = (50, 50, 255)
 def initialise_gpio():
     gpio.setmode(gpio.BOARD)
 
-    led_pins = [red_pin, amber_pin, green_pin, blue_pin, white_pin]
-    
-    for pin in led_pins:
+    for pin in [red_pin, amber_pin, green_pin, blue_pin, white_pin]:
         gpio.setup(pin, gpio.OUT)
         gpio.output(pin, gpio.LOW)
 
-    for pin in pins7:
+    for pin in [a_pin, b_pin, c_pin, d_pin, e_pin, f_pin, g_pin]:
         gpio.setup(pin, gpio.OUT)
         gpio.output(pin, gpio.HIGH)     # Active Low
+
 
 def initialise_window():
     pygame.init()
@@ -79,13 +85,13 @@ buttons     = [
     PinButton(440,  20, Pin(blue_pin),  'Blue',  button_font, lblue, off, 120, 75, 25, 22),
     PinButton(580,  20, Pin(white_pin), 'White', button_font, white, off, 120, 75, 22, 22),
 
-    PinButton( 50, 150, Pin(pins7[0], False, True), 'a', button_font, red, off, 150,  30, 60,  0),
-    PinButton( 35, 190, Pin(pins7[1], False, True), 'b', button_font, red, off,  30, 150,  7, 50),
-    PinButton(190, 190, Pin(pins7[2], False, True), 'c', button_font, red, off,  30, 150,  7, 50),
-    PinButton( 50, 350, Pin(pins7[6], False, True), 'g', button_font, red, off, 150,  30, 60,  0),
-    PinButton( 35, 390, Pin(pins7[3], False, True), 'd', button_font, red, off,  30, 150,  7, 50),
-    PinButton(190, 390, Pin(pins7[4], False, True), 'e', button_font, red, off,  30, 150,  7, 50),
-    PinButton( 50, 550, Pin(pins7[5], False, True), 'f', button_font, red, off, 150,  30, 64,  0),
+    PinButton( 50, 150, Pin(a_pin, False, True), 'a', button_font, red, off, 150,  30, 60,  0),
+    PinButton( 35, 190, Pin(b_pin, False, True), 'b', button_font, red, off,  30, 150,  7, 50),
+    PinButton(190, 190, Pin(c_pin, False, True), 'c', button_font, red, off,  30, 150,  7, 50),
+    PinButton( 50, 350, Pin(g_pin, False, True), 'g', button_font, red, off, 150,  30, 60,  0),
+    PinButton( 35, 390, Pin(d_pin, False, True), 'd', button_font, red, off,  30, 150,  7, 50),
+    PinButton(190, 390, Pin(e_pin, False, True), 'e', button_font, red, off,  30, 150,  7, 50),
+    PinButton( 50, 550, Pin(f_pin, False, True), 'f', button_font, red, off, 150,  30, 64,  0),
 ]
 
 # Event Loop
