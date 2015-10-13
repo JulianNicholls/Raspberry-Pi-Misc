8521aa848f7e6493ea35972d19f402a765d207c2
Julian Nicholls <juliannicholls29@gmail.com>
2015-10-13 20:47:46 +0100

    Add v4 graph driven by slider.

--- node/blynk-test.js
+++ node/blynk-test.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var Blynk = require('blynk-library');
 var AUTH  = 'ff55533a7af8492e83f77a6d434cc01a';
 
@@ -5,12 +7,19 @@ var blynk = new Blynk.Blynk(AUTH, options = {
     certs_path: './certs/'
 });
 
+var value   = 0;
 var v1      = new blynk.VirtualPin(1);
+var v4      = new blynk.VirtualPin(4);
 var v9      = new blynk.VirtualPin(9);
 var term    = new blynk.WidgetTerminal(3);
 
 v1.on('write', function(param) {
-    console.log('V1: ', param[0]);
+    value = param[0];
+    console.log('V1: ', value);
+});
+
+v4.on('read', function() {
+    v4.write(value);
 });
 
 v9.on('read', function() {
@@ -21,4 +30,7 @@ term.on('write', function(data) {
     term.write('You wrote: ' + data + '\n');
     blynk.notify('HAHA! ' + data);
 });
+
+blynk.on('connect', function() { console.log("Blynk ready."); });
+blynk.on('disconnect', function() { console.log("DISCONNECT"); });
 
