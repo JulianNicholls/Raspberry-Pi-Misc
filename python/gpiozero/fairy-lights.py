1012c828f154aa5ccfc6ad549ae9ed8d1503db08
Julian Nicholls <juliannicholls29@gmail.com>
2015-12-14 21:03:48 +0000

    Add options for fading and flashing fairy lights

--- python/gpiozero/fairy-lights.py
+++ python/gpiozero/fairy-lights.py
@@ -1,18 +1,48 @@
 #!/usr/bin/env python3
 
-from gpiozero import LED, Button
-from signal import pause
+from gpiozero import PWMLED, Button
 import time
+import random
 
-led = LED(18)   # GPIO Number
-button = Button(21)
+led = PWMLED(18)   # GPIO Number
+# button = Button(21)
+
+def fade_in_out():
+    for i in range(3):
+        fade_in()
+        fade_out()
+
+def fade_in():
+    led.off()
+    for i in range(20):
+        led.value = i / 20
+        time.sleep(0.1)
+
+def fade_out():
+    led.on()
+    for i in range(20):
+        led.value = (19 - i) / 20
+        time.sleep(0.1)
+
+def flash():
+    led.blink(on_time=0.3, off_time=0.3, n=10, background=False)
+
+def fast_flash():
+    led.blink(on_time=0.15, off_time=0.15, n=20, background=False)
 
 while True:
-    button.wait_for_press()
-    led.on()
-    button.wait_for_release()
+    which = random.randrange(5)
 
-    button.wait_for_press()
-    led.off()
-    button.wait_for_release()
+    if which == 0:
+        fade_in_out()
+    elif which == 1:
+        fade_in()
+        fade_out()
+    elif which == 2:
+        fade_out()
+        fade_in()
+    elif which == 3:
+        flash()
+    elif which == 4:
+        fast_flash()
 
