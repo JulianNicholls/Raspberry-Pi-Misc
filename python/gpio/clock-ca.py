8e3e74642a1decb9cc94989613022774cd0e3e6f
Julian Nicholls <juliannicholls29@gmail.com>
2016-02-06 19:41:21 +0000

    Update clock-ca.py to more idiomatic Python

--- python/gpio/clock-ca.py
+++ python/gpio/clock-ca.py
@@ -16,52 +16,56 @@ segments = (11, 4, 23, 8, 7, 10, 18, 25)
 
 digits = (22, 27, 17, 24)
 
-# These are actually reversed for a common anode quad 7 segment display
+# Confusingly,
+#   HIGH = Segment OFF
+#   LOW  = Segment ON
 
 num = {
-    ' ': (0, 0, 0, 0, 0, 0, 0), 
-    '0': (1, 1, 1, 1, 1, 1, 0), 
-    '1': (0, 1, 1, 0, 0, 0, 0), 
-    '2': (1, 1, 0, 1, 1, 0, 1), 
-    '3': (1, 1, 1, 1, 0, 0, 1), 
-    '4': (0, 1, 1, 0, 0, 1, 1), 
-    '5': (1, 0, 1, 1, 0, 1, 1), 
-    '6': (1, 0, 1, 1, 1, 1, 1), 
-    '7': (1, 1, 1, 0, 0, 0, 0), 
-    '8': (1, 1, 1, 1, 1, 1, 1), 
-    '9': (1, 1, 1, 1, 0, 1, 1)
+    ' ': (GPIO.HIGH, GPIO.HIGH, GPIO.HIGH, GPIO.HIGH, GPIO.HIGH, GPIO.HIGH, GPIO.HIGH),
+    '0': (GPIO.LOW,  GPIO.LOW,  GPIO.LOW,  GPIO.LOW,  GPIO.LOW,  GPIO.LOW,  GPIO.HIGH),
+    '1': (GPIO.HIGH, GPIO.LOW,  GPIO.LOW,  GPIO.HIGH, GPIO.HIGH, GPIO.HIGH, GPIO.HIGH),
+    '2': (GPIO.LOW,  GPIO.LOW,  GPIO.HIGH, GPIO.LOW,  GPIO.LOW,  GPIO.HIGH, GPIO.LOW),
+    '3': (GPIO.LOW,  GPIO.LOW,  GPIO.LOW,  GPIO.LOW,  GPIO.HIGH, GPIO.HIGH, GPIO.LOW),
+    '4': (GPIO.HIGH, GPIO.LOW,  GPIO.LOW,  GPIO.HIGH, GPIO.HIGH, GPIO.LOW,  GPIO.LOW),
+    '5': (GPIO.LOW,  GPIO.HIGH, GPIO.LOW,  GPIO.LOW,  GPIO.HIGH, GPIO.LOW,  GPIO.LOW),
+    '6': (GPIO.LOW,  GPIO.HIGH, GPIO.LOW,  GPIO.LOW,  GPIO.LOW,  GPIO.LOW,  GPIO.LOW),
+    '7': (GPIO.LOW,  GPIO.LOW,  GPIO.LOW,  GPIO.HIGH, GPIO.HIGH, GPIO.HIGH, GPIO.HIGH),
+    '8': (GPIO.LOW,  GPIO.LOW,  GPIO.LOW,  GPIO.LOW,  GPIO.LOW,  GPIO.LOW,  GPIO.LOW),
+    '9': (GPIO.LOW,  GPIO.LOW,  GPIO.LOW,  GPIO.LOW,  GPIO.HIGH, GPIO.LOW,  GPIO.LOW)
 }
  
+# Strobe a select pin high
+
+def strobe_high(pin):
+    GPIO.output(pin, GPIO.HIGH)
+    time.sleep(0.001)
+    GPIO.output(pin, GPIO.LOW)
+
+
 # Initialise the GPIO ports
 
 GPIO.setmode(GPIO.BCM)
 
 for segment in segments:
-    GPIO.setup(segment, GPIO.OUT)
-    GPIO.output(segment, 1)
+    GPIO.setup(segment, GPIO.OUT, initial=GPIO.HIGH)
 
 for digit in digits:
-    GPIO.setup(digit, GPIO.OUT)
-    GPIO.output(digit, 0)
+    GPIO.setup(digit, GPIO.OUT, initial=GPIO.LOW)
  
 try:
     while True:
         _time = time.ctime()
-        n = _time[11:13] + _time[14:16]
-        s = str(n).rjust(4)
+        tmstr = _time[11:13] + _time[14:16]
+        secs  = int(time.ctime()[18:19])
 
         for digit in range(4):
-            if(int(time.ctime()[18:19]) % 2 == 0) and (digit == 1):
-                GPIO.output(25, 0)
-            else:
-                GPIO.output(25, 1)
+            colon = GPIO.LOW if (digit == 1 and secs % 2 == 0) else GPIO.HIGH
+            GPIO.output(25, colon)
 
             for loop in range(7):
-                GPIO.output(segments[loop], 1 - num[s[digit]][loop])
+                GPIO.output(segments[loop], num[tmstr[digit]][loop])
 
-            GPIO.output(digits[digit], 1)
-            time.sleep(0.001)
-            GPIO.output(digits[digit], 0)
+            strobe_high(digits[digit])
 
 finally:
     GPIO.cleanup()
